691695c59e64385b06b0e3abb89303480fdbfe2a
Daniil <dankon368024@gmail.com>
2022-05-22 21:22:55 +0300

    V0.15

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,9 +1,12 @@
 #from email import message
+from cgitb import text
+from posixpath import split
+import string
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-import os 
+import os, json, string
 
 
 
@@ -33,11 +36,9 @@ async def pizza_place_command(message : types.Message):
 
 @dp.message_handler()
 async def echo_send(message: types.Message):
-    await message.answer(message.text)
-    #await message.reply(message.text)    
-    #await bot.send_message(message.from_user.id, message.text)
-
-
-
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')}\
+        .intersection(set(json.load(open('cenz.json')))) != set():
+        await message.reply('Маты запрещены')
+        await message.delete()
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
