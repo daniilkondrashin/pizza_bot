e5f6c929defef09d03e4b4d6dea446248c7b5249
Daniil <dankon368024@gmail.com>
2022-07-11 22:09:07 +0300

    V0.21

--- handlers/client.py
+++ handlers/client.py
@@ -1,10 +1,12 @@
+
 from aiogram import Dispatcher, types
 from create_bot import dp, bot
+from keyboards import kb_client
 
-#@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start', 'help'])
 async def command_start(message : types.Message):
     try:
-        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        await bot.send_message(message.from_user.id, 'Приятного аппетита', reply_markup=kb_client)
         await message.delete()
     except:
         await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/my_telegram_pizzaBot')
